6db8dd86d62ec98963e90e73d00d3958d7d0ee9f
lakshmanganta <glaxman9496@gmail.com>
2018-09-20 18:12:41 +0530

    java file

---
+++ testGit/src/testGit/gitClass.java
@@ -0,0 +1,12 @@
+package testGit;
+
+public class gitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World......");
+
+	}
+
+}
